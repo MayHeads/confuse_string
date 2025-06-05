44a3a3567fcbef971cf8978b7d52a90e299e7b4c
gyqin <qingguangyi@xmiles.cn>
2025-06-05 11:50:52 +0800

    - 测试案例

--- ConfuseDemo1/ConfuseDemo1/ViewController.swift
+++ ConfuseDemo1/ConfuseDemo1/ViewController.swift
@@ -32,3 +32,25 @@ class ViewController: UIViewController {
 
 }
 
+
+class NoteGuideModel {
+    var souce: String
+    var title: String
+    var tips: String
+    var indiImage: String
+    
+    init(souce: String, title: String, tips: String, indiImage: String) {
+        self.souce = souce
+        self.title = title
+        self.tips = tips
+        self.indiImage = indiImage
+    }
+     
+    static let allItems: [NoteGuideModel] = [
+        NoteGuideModel.init(souce: "3s", title: "gu1", tips: "gut1", indiImage: "indi_1"),
+        NoteGuideModel.init(souce: "cleansame", title: "gu2", tips: "gut2", indiImage: "indi_2"),
+        NoteGuideModel.init(souce: "cleanbigvideo", title: "gu3", tips: "gut3", indiImage: "indi_3"),
+    ]
+}
+
+
